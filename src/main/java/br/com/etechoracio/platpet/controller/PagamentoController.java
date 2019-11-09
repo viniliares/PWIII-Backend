b2a7e22f841c683f0ff1728c54f544ffe4130b9c
Vinícius <viniciusjb2013@hotmail.com>
2019-11-08 22:19:07 -0300

    API com DELETE - Vinícius e Rogério

--- src/main/java/br/com/etechoracio/platpet/controller/PagamentoController.java
+++ src/main/java/br/com/etechoracio/platpet/controller/PagamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,16 @@ public class PagamentoController {
 				Pagamento updated = dao.save(record);
 				return ResponseEntity.ok().body(updated);
 			}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@DeleteMapping(path= {"/{id}"})
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id){
+		return dao.findById(id)
+				.map(record -> {
+					dao.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
 
